56f3237fbd891dd2961a1ca0889d51e6ca97f01f
Lynette <lynettecargo@Lynettes-Air.home>
2023-08-09 14:08:30 -0500

    added images

--- app.js
+++ app.js
@@ -15,8 +15,27 @@ function debounce(func, wait = 20, immediate = true) {
 
   const sliderImages = document.querySelectorAll('.slide-in')
 
-  function checkSlide(e) {
-    console.log(e);
+//   loop over every image
+  function checkSlide() {
+    // console.log(window.scrollY);
+    // --- halfway through the image
+    sliderImages.forEach(slideImage => {
+        const slideInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+        // console.log(slideInAt);
+        // bottom of image - slide up
+        // ----- bottom of the image
+        const imageBottom = slideImage.offsetTop + slideImage.height;
+        console.log();
+        // if half shown / not scroll past
+        const isHalfShown = slideInAt > slideImage.offsetTop;
+        const isNotScrollPast = window.scrollY < imageBottom
+
+        if(isHalfShown && isNotScrollPast){
+            slideImage.classList.add('active')
+        }else{
+            slideImage.classList.remove('active')
+        }
+    })
   }
 
   window.addEventListener('scroll', debounce(checkSlide));
